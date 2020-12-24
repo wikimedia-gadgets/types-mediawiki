00fd1d43203e1321ded8c97445f3ed852a4f2cfa
Siddharth VP <siddharthvp@gmail.com>
2020-12-25 00:17:01 +0530

    Add api params types
    Generated using a script.
    
    Helps improve Api.d.ts – no too many `any`s now.

--- mw/Api.d.ts
+++ mw/Api.d.ts
@@ -1,4 +1,14 @@
+import {
+	ApiEditPageParams,
+	ApiParseParams,
+	ApiQueryAllMessagesParams,
+	ApiRollbackParams,
+	ApiUploadParams
+} from "../api_params";
+
 type title = string | mw.Title
+type ApiParams = Record<string, string | string[] | boolean | number | number[]>
+type ApiResponse = Record<string, any> // it will always be a JSON object, the rest is uncertain ...
 
 interface ApiOptions {
 	parameters?: Record<string, string>
@@ -6,7 +16,7 @@ interface ApiOptions {
 	useUS?: boolean
 }
 interface ForeignApiOptions extends ApiOptions {
-	anonymous: boolean
+	anonymous?: boolean
 }
 
 declare global {
@@ -16,36 +26,38 @@ declare global {
 
 			abort(): void
 
-			get(parameters: any, ajaxOptions?: JQuery.AjaxSettings): JQuery.Promise<any>
+			get(parameters: ApiParams, ajaxOptions?: JQuery.AjaxSettings): JQuery.Promise<ApiResponse>
 
-			post(parameters: any, ajaxOptions?: JQuery.AjaxSettings): JQuery.Promise<any>
+			post(parameters: ApiParams, ajaxOptions?: JQuery.AjaxSettings): JQuery.Promise<ApiResponse>
 
-			preprocessParameters(parameters: any, useUS: boolean): void
+			preprocessParameters(parameters: ApiParams, useUS: boolean): void
 
 			// index.js
-			ajax(parameters: any, ajaxOptions?: JQuery.AjaxSettings): JQuery.Promise<any>
+			ajax(parameters: ApiParams, ajaxOptions?: JQuery.AjaxSettings): JQuery.Promise<ApiResponse>
 
-			postWithToken(tokenType: string, params: any, ajaxOptions?: JQuery.AjaxSettings): JQuery.Promise<any>
+			postWithToken(tokenType: string, params: ApiParams, ajaxOptions?: JQuery.AjaxSettings)
+				: JQuery.Promise<ApiResponse>
 
-			getToken(type: string, additionalParams?: any): JQuery.Promise<string>
+			getToken(type: string, additionalParams?: ApiParams): JQuery.Promise<string>
 
 			badToken(type: string): void
 
-			getErrorMessage(data: any): JQuery
+			getErrorMessage(data: ApiResponse): JQuery
 
 			// edit.js
-			postWithEditToken(params: any, ajaxOptions?: JQuery.AjaxSettings): JQuery.Promise<any>
+			postWithEditToken(params: ApiParams, ajaxOptions?: JQuery.AjaxSettings): JQuery.Promise<ApiResponse>
 
 			getEditToken(): JQuery.Promise<string>
 
-			create(title: title, params: any, content: string): JQuery.Promise<any>
+			create(title: title, params: ApiEditPageParams, content: string): JQuery.Promise<ApiResponse>
 
 			edit(title: title, transform: (data: {
 				timestamp: string,
 				content: string
-			}) => any): JQuery.Promise<any>
+			}) => string | ApiEditPageParams): JQuery.Promise<any>
 
-			newSection(title: title, header: string, message: string, additionalParams?: any): JQuery.Promise<any>
+			newSection(title: title, header: string, message: string, additionalParams?: ApiEditPageParams)
+				: JQuery.Promise<ApiResponse>
 
 			// user.js
 			getUserInfo(): JQuery.Promise<{
@@ -53,15 +65,15 @@ declare global {
 				rights: string[]
 			}>
 
-			assertCurrentUser(query: any): JQuery.Promise<{
+			assertCurrentUser(query: ApiParams): JQuery.Promise<{
 				assert: 'anon' | 'user'
 				assertUser: string
 			}>
 
 			// options.js
-			saveOption(name: string, value: string): JQuery.Promise<any>
+			saveOption(name: string, value: string): JQuery.Promise<ApiResponse>
 
-			saveOptions(options: { [optionName: string]: string }): JQuery.Promise<any>
+			saveOptions(options: { [optionName: string]: string }): JQuery.Promise<ApiResponse>
 
 			// watch.js
 			watch(pages: title | title[]): JQuery.Promise<{
@@ -73,14 +85,14 @@ declare global {
 			}>
 
 			// parse.js
-			parse(content: string | mw.Title, additionalParams?: any): JQuery.Promise<any>
+			parse(content: string | mw.Title, additionalParams?: ApiParseParams): JQuery.Promise<ApiResponse>
 
 			// messages.js
-			getMessages(messages: string[], options?: any): JQuery.Promise<any>
+			getMessages(messages: string[], options?: ApiQueryAllMessagesParams): JQuery.Promise<ApiResponse>
 
-			loadMessages(messages: string[], options?: any): JQuery.Promise<any>
+			loadMessages(messages: string[], options?: ApiQueryAllMessagesParams): JQuery.Promise<ApiResponse>
 
-			loadMessagesIfMissing(messages: string[], options?: any): JQuery.Promise<any>
+			loadMessagesIfMissing(messages: string[], options?: ApiQueryAllMessagesParams): JQuery.Promise<ApiResponse>
 
 			// category.js
 			isCategory(title: title): JQuery.Promise<boolean>
@@ -90,24 +102,26 @@ declare global {
 			getCategories(title: title): JQuery.Promise<boolean | mw.Title[]>
 
 			// rollback.js
-			rollback(page: title, user: string, params?: any): JQuery.Promise<any>
+			rollback(page: title, user: string, params?: ApiRollbackParams): JQuery.Promise<any>
 
 			// upload.js
-			chunkedUpload(file: File, data: any, chunkSize?: number, chunkRetries?: number): JQuery.Promise<any>
+			chunkedUpload(file: File, data: ApiUploadParams, chunkSize?: number, chunkRetries?: number)
+				: JQuery.Promise<ApiResponse>
 
-			chunkedUploadToStash(file: File, data?: any, chunkSize?: number, chunkRetries?: number): JQuery.Promise<any>
+			chunkedUploadToStash(file: File, data?: ApiUploadParams, chunkSize?: number, chunkRetries?: number)
+				: JQuery.Promise<ApiResponse>
 
-			upload(file: File | Blob | HTMLInputElement, data: any): JQuery.Promise<any>
+			upload(file: File | Blob | HTMLInputElement, data: ApiUploadParams): JQuery.Promise<ApiResponse>
 
-			uploadFromStash(filekey: string, data: any): JQuery.Promise<any>
+			uploadFromStash(filekey: string, data: ApiUploadParams): JQuery.Promise<ApiResponse>
 
-			uploadToStash(file: File | HTMLInputElement, data?: any): JQuery.Promise<any>
+			uploadToStash(file: File | HTMLInputElement, data?: ApiUploadParams): JQuery.Promise<ApiResponse>
 
 			// login.js
-			login(username: string, password: string): JQuery.Promise<any>
+			login(username: string, password: string): JQuery.Promise<ApiResponse>
 		}
 		class ForeignApi extends mw.Api {
-			constructor(url: string | mw.Uri, options: ForeignApiOptions)
+			constructor(url: string | mw.Uri, options?: ForeignApiOptions)
 
 			getOrigin(): string | void
 		}
---
+++ scripts/api-types-generator.js
@@ -0,0 +1,71 @@
+// Paste this into the browser console
+// and copy the console log output
+
+const data = await new mw.Api().get({
+	"action": "paraminfo",
+	"format": "json",
+	"modules": "*",
+	"formatversion": "2"
+})
+
+const queryApiData = await new mw.Api().get({
+	"action": "paraminfo",
+	"format": "json",
+	"modules": "query+*",
+	"formatversion": "2"
+})
+
+function processParamInfo(type, name, multi) {
+	if (Array.isArray(type)) {
+		type = type.map(e => `'${e}'`).join(' | ')
+		if (multi) {
+			type = `(${type})[]`
+		}
+	} else {
+		// API uses type=text for long string fields
+		if (type === 'text' || type === 'title' || type === 'user') {
+			type = 'string'
+		} else if (type === 'integer') {
+			type = 'number'
+		}
+		if (multi) {
+			type = `${type} | ${type}[]`
+		}
+	}
+
+	// Avoid being over-specific
+	if (name === 'tags' || name === 'tagfilter' || // edit tags, used in core
+		name === 'wikis' || // used by Extension:Echo APIs
+		name === 'site' // gusite used by ApiQueryGlobalUsage
+	) {
+		type = 'string | string[]'
+	} else if (name.includes(`-`)) {
+		name = `'${name}'`
+	}
+	return {name, type}
+}
+
+function getInterfaceName(module) {
+	return module.classname.replace(/\\/g, '')
+		.replace(/^MediaWikiExtensions?/, '')
+		.replace(/ApiApi/g, 'Api')
+}
+
+const actionsTypes = data.paraminfo.modules.map(module => {
+	return `export interface ${getInterfaceName(module)}Params extends ApiParams {\n` +
+		module.parameters.map((param) => {
+			const {name, type} = processParamInfo(param.type, param.name, param.multi)
+			return `${name}?: ${type}`
+		}).join('\n').replace(/^/mg, '\t') + `\n}`
+}).join('\n\n')
+
+const queryTypes = queryApiData.paraminfo.modules.map(module => {
+	return `export interface ${getInterfaceName(module)}Params extends ApiQueryParams {\n` +
+		module.parameters.map((param) => {
+			const {name, type} = processParamInfo(param.type, param.name, param.multi)
+			return `${module.prefix}${name}?: ${type}`
+		}).join('\n').replace(/^/mg, '\t') + `\n}`
+}).join('\n\n')
+
+
+console.log(actionsTypes + '\n\n' + queryTypes)
