3bb2d5c035aa5105e9e6f20bf3713f0749a6458c
cAttte <26514199+cAttte@users.noreply.github.com>
2021-03-04 02:59:58 -0300

    specify mw.config types

--- mw/index.d.ts
+++ mw/index.d.ts
@@ -29,7 +29,60 @@ declare global {
          * global `window` object.
          * @see https://doc.wikimedia.org/mediawiki-core/master/js/#!/api/mw-property-config
          */
-        const config: mw.Map;
+        const config: mw.Map<{
+            debug: boolean;
+            skin: string;
+            stylepath: string;
+            wgArticlePath: string;
+            wgCaseSensitiveNamespaces: string[];
+            wgContentLanguage: string;
+            wgContentNamespaces: number[];
+            wgDBname: string;
+            wgEnableAPI: boolean;
+            wgEnableWriteAPI: boolean;
+            wgExtensionAssetsPath: string;
+            wgFormattedNamespaces: Record<string, string>;
+            wgNamespaceIds: Record<string, string>;
+            wgScript: string;
+            wgScriptPath: string;
+            wgServer: string;
+            wgSiteName: string;
+            wgVariantArticlePath: string | false;
+            wgVersion: string;
+            wgAction: string;
+            wgArticleId: number;
+            wgCanonicalNamespace: string;
+            wgCanonicalSpecialPageName: string | false;
+            wgCategories: string[];
+            wgCurRevisionId: number;
+            wgIsArticle: boolean;
+            wgIsProbablyEditable: boolean;
+            wgNamespaceNumber: number;
+            wgPageContentLanguage: string;
+            wgPageContentModel: string;
+            wgPageName: string;
+            wgRedirectedFrom: string;
+            wgRelevantPageName: string;
+            wgRelevantUserName: string;
+            wgRelevantPageIsProbablyEditable: boolean;
+            wgRestrictionEdit: string[];
+            wgRestrictionMove: string[];
+            wgRevisionId: number;
+            wgSearchType: string;
+            wgTitle: string;
+            wgUserEditCount: number;
+            wgUserGroups: string[];
+            wgUserId: number;
+            wgUserLanguage: string;
+            wgUserName: string;
+            wgUserRegistration: number;
+            wgIsMainPage: boolean;
+            wgUserVariant: string;
+            wgPostEdit: string;
+            wgDiffOldId: number;
+            wgDiffNewId: number;
+            wgWikibaseItemId: string;
+        }>;
 
         namespace html {
             function escape(s: string): string;
