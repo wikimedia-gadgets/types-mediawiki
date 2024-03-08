972ad3c8383dcdbacb6b46da63b3fa1996066ed4
Adrien LESÉNÉCHAL <adrien.lesenechal@etu.univ-nantes.fr>
2024-03-08 19:13:27 +0100

    Add types for JQuery plugin of `mediawiki.htmlform`

--- jquery/index.d.ts
+++ jquery/index.d.ts
@@ -6,6 +6,7 @@ import "./colorUtil";
 import "./confirmable";
 import "./footHovzer";
 import "./highlightText";
+import "./htmlform";
 import "./lengthLimit";
 import "./makeCollapsible";
 import "./spinner";
