581f2b16c343f1c1a8bda19e28c8e8fd11844d8f
Adrien LESÉNÉCHAL <adrien.lesenechal@etu.univ-nantes.fr>
2024-03-08 17:06:27 +0100

    Update JQuery imports

--- jquery/index.d.ts
+++ jquery/index.d.ts
@@ -1,6 +1,13 @@
 import "jquery";
 
-import "./textSelection";
-import "./collapsibleTabs";
 import "./client";
+import "./collapsibleTabs";
 import "./colorUtil";
+import "./confirmable";
+import "./highlightText";
+import "./lengthLimit";
+import "./makeCollapsible";
+import "./spinner";
+import "./tablesorter";
+import "./textSelection";
+import "./updateTooltipAccessKeys";
